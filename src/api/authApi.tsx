cdd95d7e1697e6de86a2d5f4714202cad646ac51
iAmShivay <shivaysharma77893@gmail.com>
2024-06-19 15:00:45 +0530

    Production Ready

--- src/api/authApi.tsx
+++ src/api/authApi.tsx
@@ -1,4 +1,9 @@
-import axios from "axios";
+// api.ts
+
+import axios from 'axios';
+
+// Accessing the environment variable
+const API_URL = import.meta.env.VITE_API_URL;
 
 interface Credentials {
   firstName: string;
@@ -7,33 +12,36 @@ interface Credentials {
   password: string;
 }
 
-const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;  // Fallback to local URL if env var is not set
-
 export const Register = async (credentials: Credentials) => {
   try {
-    const response = await axios.post(`${apiBaseUrl}/register`, credentials);
+    const response = await axios.post(`${API_URL}/register`, credentials);
     console.log("Registration response:", response.data);
     return response.data;
   } catch (error: any) {
     if (error.response) {
       const errorMessage = error.response.data.message;
-      console.log(errorMessage);
-      throw errorMessage;
+      console.error("Registration error:", errorMessage);
+      throw new Error(errorMessage);
+    } else {
+      console.error("Registration error:", error.message);
+      throw new Error("An unexpected error occurred.");
     }
-    return error;
   }
 };
 
 export const Login = async (credentials: { email: string; password: string }) => {
   try {
-    const response = await axios.post(`${apiBaseUrl}/login`, credentials);
+    const response = await axios.post(`${API_URL}/login`, credentials);
     console.log("Login response:", response.data);
     return response.data;
   } catch (error: any) {
     if (error.response) {
       const errorMessage = error.response.data.message;
-      console.log(errorMessage);
-      throw errorMessage;
+      console.error("Login error:", errorMessage);
+      throw new Error(errorMessage);
+    } else {
+      console.error("Login error:", error.message);
+      throw new Error("An unexpected error occurred.");
     }
   }
 };
