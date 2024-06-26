a3acb14dc8bb01493378a96bc63c7db7a203ac6b
iAmShivay <shivaysharma77894@gmail.com>
2024-06-26 11:14:17 +0530

    Added Admin Link

--- src/components/Section/Login/Login.tsx
+++ src/components/Section/Login/Login.tsx
@@ -1,150 +1,3 @@
-// import React, { useState, ChangeEvent } from "react";
-// import {
-//   TextField,
-//   Button,
-//   Container,
-//   Box,
-//   Typography,
-//   Paper,
-//   Link,
-//   CircularProgress,
-// } from "@mui/material";
-// import { Link as RouterLink } from "react-router-dom";
-// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-// import { useDispatch } from "react-redux";
-// import { loginUser } from "../../../app/auth/authSlice";
-// import { AppDispatch } from "../../../store";
-// const LoginPage: React.FC = () => {
-//   const dispatch = useDispatch<AppDispatch>();
-//   const [submitting, setSubmitting] = useState<boolean>(false);
-
-//   interface FormState {
-//     email: string;
-//     password: string;
-//   }
-
-//   const [formData, setFormData] = useState<FormState>({
-//     email: "",
-//     password: "",
-//   });
-
-//   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = e.target;
-//     setFormData((prevData) => ({
-//       ...prevData,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleSubmit = async () => {
-//     setSubmitting(true);
-//     try {
-//       dispatch(loginUser(formData));
-//     } catch (err) {
-//       console.log(err);
-//       // Handle registration error here, you can set an error state and display it to the user
-//     } finally {
-//       setSubmitting(false);
-//       setFormData({
-//         email: "",
-//         password: "",
-//       });
-//     }
-//   };
-//   return (
-//     <Container
-//       maxWidth="xs"
-//       style={{
-//         marginTop: "64px",
-//       }}
-//     >
-//       <Paper
-//         elevation={6}
-//         style={{
-//           padding: "32px",
-//           borderRadius: "8px",
-//         }}
-//       >
-//         <Box
-//           style={{
-//             display: "flex",
-//             flexDirection: "column",
-//             alignItems: "center",
-//           }}
-//         >
-//           <ShoppingCartIcon
-//             color="primary"
-//             style={{
-//               fontSize: "40px",
-//               marginBottom: "16px",
-//             }}
-//           />
-//           <Typography variant="h5" gutterBottom>
-//             Welcome to E-Shop
-//           </Typography>
-//           <Typography variant="subtitle1" gutterBottom>
-//             Please login to continue
-//           </Typography>
-//           <TextField
-//             label="Email"
-//             name="email"
-//             variant="outlined"
-//             fullWidth
-//             margin="normal"
-//             value={formData.email}
-//             onChange={handleChange}
-//             style={{
-//               marginBottom: "16px",
-//             }}
-//           />
-//           <TextField
-//             label="Password"
-//             name="password"
-//             variant="outlined"
-//             type="password"
-//             fullWidth
-//             margin="normal"
-//             value={formData.password}
-//             onChange={handleChange}
-//             style={{
-//               marginBottom: "16px",
-//             }}
-//           />
-//           <Button
-//             variant="contained"
-//             color="primary"
-//             onClick={handleSubmit}
-//             disabled={submitting}
-//             sx={{ position: "relative" }}
-//           >
-//             {submitting ? (
-//               <CircularProgress size={24} sx={{ color: "white" }} />
-//             ) : (
-//               "Submit"
-//             )}
-//           </Button>
-
-//           <Typography
-//             variant="body2"
-//             align="center"
-//             style={{ marginTop: "16px" }}
-//           >
-//             Don,t have an account?{" "}
-//             <Link component={RouterLink} to="/register">
-//               Register Now
-//             </Link>
-//           </Typography>
-//         </Box>
-//       </Paper>
-//     </Container>
-//   );
-// };
-
-// export default LoginPage;
-
-
-
-
 import React, { useState, ChangeEvent } from "react";
 import {
   TextField,
@@ -204,8 +57,21 @@ const LoginPage: React.FC = () => {
   return (
     <Container maxWidth="lg" style={{ marginTop: "64px" }}>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <img src="/images/Login.jpg" alt="Education Consultancy" style={{ maxWidth: "100%", height: "60vh" }} />
+        <Grid
+          item
+          xs={12}
+          md={6}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="/images/Login.jpg"
+            alt="Education Consultancy"
+            style={{ maxWidth: "100%", height: "60vh" }}
+          />
         </Grid>
         <Grid item xs={12} md={6}>
           <Paper elevation={6} style={{ padding: "32px", borderRadius: "8px" }}>
@@ -277,6 +143,16 @@ const LoginPage: React.FC = () => {
                   Register Now
                 </Link>
               </Typography>
+              <Typography
+                variant="body2"
+                align="center"
+                style={{ marginTop: "16px" }}
+              >
+                Are You An Admin?{" "}
+                <Link component={RouterLink} to="/admin/dashboard">
+                  Go To Admin Dashboard
+                </Link>
+              </Typography>
             </Box>
           </Paper>
         </Grid>
@@ -286,6 +162,3 @@ const LoginPage: React.FC = () => {
 };
 
 export default LoginPage;
-
-
-
